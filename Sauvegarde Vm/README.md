ac12b2367ff6656d7b24171575328bebef4d3364
lteddy <teddy.lemoal@ynov.com>
2024-12-02 10:41:59 +0100

    sauveagrde

---
+++ Sauvegarde Vm/README.md
@@ -0,0 +1,15 @@
+# Rendu VM
+
+
+- SSH Ok
+- Update Ok
+- Gwet Ok
+
+-----------------------------
+SSH : usersteam@10.7.1.4
+MDP : 123
+
+
+-------------------------
+Hostname : "LinuxGSM
+MDP : adminb1FjIVhV
---
+++ Sauvegarde Vm/machine.sh
@@ -0,0 +1,34 @@
+sudo adduser usersteam
+sudo echo "DEVICE=enp0s8" >> /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo echo "NAME=lan" >> /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo echo "ONBOOT=yes" >> /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo echo "BOOTPROTO=static" >> /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo echo "IPADDR=10.7.1.4" >> /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo echo "NETMASK=255.255.255.0" >> /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo echo "DNS1=1.1.1.1" >> /etc/sysconfig/network-scripts/ifcfg-enp0s8
+sudo nmcli connection reload
+sudo nmcli connection up lan
+sudo firewall-cmd --add-masquerade --permanent
+sudo firewall-cmd --reload
+sudo firewall-cmd --permanent --remove-service dhcpv6-client
+sudo firewall-cmd --permanent --remove-service cockpit
+sudo firewall-cmd --reload
+sudo firewall-cmd --permanent --add-port=22/tcp
+sudo firewall-cmd --reload
+su usersteam
+sudo hostnamectl set-hostname ServerGmod
+sudo passwd usersteam
+ -> Comment on fait pour mettre un mdp ? (123)
+sudo systemctl restart sshd
+sudo dnf update 
+sudo dnf install wget
+su root
+wget https://linuxgsm.com/dl/linuxgsm.sh 
+chmod +x linuxgsm.sh
+su usersteam
+bash linuxgsm.sh gmodserver
+./gmodserver install
+
+
+
+
